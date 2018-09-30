4716913d5eabf71c77c54a1e92be4ed9ffa394c0
Gavin Gavington <GavinGavington@gavins-macbook-air.local>
2018-09-30 11:35:15 -0700

    Initial Commit

---
+++ HiLo/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  HiLo
+//
+//  Created by Gavin Gavington on 9/30/18.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
